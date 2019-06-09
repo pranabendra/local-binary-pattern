fa9ce789bc298fc3045185e1a440785f77c9d959
pranabendra <30474395+pranabendra@users.noreply.github.com>
2019-06-09 14:57:48 +0530

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#==========================================
+# Title :   Initialize directory
+# Author:   pranabendra
+# Date  :   11 Feb 2019
+#==========================================
+# -*- coding: utf-8 -*-
